f167c9d5f7c04b78a9a0a44097167aea7e7666e8
hataketsu <hataketsu@gmail.com>
2018-09-28 16:28:27 +0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,77 @@
+#include <Arduino.h>
+#include "ESP8266WiFi.h"
+
+const char *ssid = "Franxx";
+const char *password = "nopenope";
+
+WiFiServer wifiServer(80);
+
+void connectWifi();
+
+void closeClient(WiFiClient &client);
+
+const int ALL_PINS[] = {D0, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10};
+
+static const int READ_PINS = 6;
+
+static const int WRITE_PINS = 8;
+
+void setup() {
+
+    Serial.begin(115200);
+
+    delay(1000);
+
+    connectWifi();
+
+    wifiServer.begin();
+}
+
+void connectWifi() {
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting..");
+    }
+
+    Serial.print("Connected to WiFi. IP:");
+    Serial.println(WiFi.localIP());
+}
+
+void loop() {
+    if (WiFi.status() != WL_CONNECTED) //if we get disconnected from wifi, try to reconnect
+        connectWifi();
+
+    WiFiClient client = wifiServer.available();
+
+    if (client) { //has new connected client?
+
+        while (client.connected()) {
+            delay(200); //wait for client done sending
+            int command = client.read();
+            if (command == READ_PINS) {
+                for (int pin : ALL_PINS) { //send all digital pins' status
+                    client.write(digitalRead(pin));
+                }
+                client.write(analogRead(A0) * 256 / 1024); //send analog pin value, fit it into one byte
+            } else if (command == WRITE_PINS) {
+                int address = client.read(); //address of digital pin from 0-11. analog = 100
+                int value = client.read();
+                if (address == 100) {
+                    analogWrite(A0, value * 1024 / 256);
+                } else {
+                    digitalWrite(ALL_PINS[address], value);
+                }
+            }
+            delay(10);
+        }
+
+        closeClient(client);
+    }
+}
+
+void closeClient(WiFiClient &client) {
+    client.stop();
+    Serial.println("Client disconnected");
+}
